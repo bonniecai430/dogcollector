305cbb85091ce84f343724aca86e2fc02745cb6d
bonnie430 <bonniecai97@gmail.com>
2022-07-12 13:25:59 -0700

    update3

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns=[
     path('',views.home,name='home'),
     path('about/',views.about,name='about'),
     path('dogs/',views.index,name='index'),
+    path('dogs/<int:dog_id>/',views.detail,name='detail')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Dog
 
 # Create your views here.
 # Define the home view
 def home(request):
-  return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+  return render(request,'home.html')
 
 def about(request):
     return render(request,'about.html')
 
 def index(request):
-    return render(request,'dogs/index.html',{'dogs':dogs})
+    dogs=Dog.objects.all()
+    return render(request,'dogs/index.html',{'dogs': dogs })
+
+def detail(request,dog_id):
+    dog= Dog.objects.get(id=dog_id)
+    return render(request,'dogs/detail.html',{'dog':dog})
